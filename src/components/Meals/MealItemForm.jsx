c3579bd2b6acbb9404a13fd4878781474adfc193
soongu <naong526@naver.com>
2025-02-14 09:15:44 +0900

    # 2. 가변속성 스프레드

--- src/components/Meals/MealItemForm.jsx
+++ src/components/Meals/MealItemForm.jsx
@@ -4,16 +4,25 @@ import styles from './MealItemForm.module.scss';
 const MealItemForm = (props) => {
   return (
     <form className={styles.form}>
+      {/* <Input
+        label={'이름'}
+        inputAttr={{
+          type: 'text',
+          onFocus: () => console.log('focus!'),
+          onChange: e => console.log(e.target.value)
+        }}
+      /> */}
+
       <Input
-        label='수량'
-        // input={{
-        //   id: 'amount_' + props.id,
-        //   type: 'number',
-        //   min: '1',
-        //   max: '5',
-        //   step: '1',
-        //   defaultValue: '1',
-        // }}
+        label='구매여부'
+        inputAttr={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
       />
       <button>담기</button>
     </form>
