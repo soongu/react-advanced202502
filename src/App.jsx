f106f9eb3f01d6f51636c52e181222dbeda255bf
soongu <naong526@naver.com>
2025-02-14 09:33:18 +0900

    # 4. 모달 열고 닫는 기능

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import Header from "./components/Layout/Header";
 import './App.css';
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import { useState } from "react";
 
 const App = () => {
-  return <>
-    <Cart />
-    <Header />
-    <div id="main">
-      <Meals />
-    </div>
-  </>;
+  // 장바구니 모달을 열고 닫는 상태변수
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  // 모달을 열어주는 함수
+  const handleShowCart = () => setCartIsShown(true);
+
+  // 모달을 닫아주는 함수
+  const handleHideCart = () => setCartIsShown(false);
+
+  return (
+    <>
+      {cartIsShown && <Cart onClose={handleHideCart} />}
+      <Header onShowCart={handleShowCart} />
+      <div id='main'>
+        <Meals />
+      </div>
+    </>
+  );
 };
 
 export default App;
