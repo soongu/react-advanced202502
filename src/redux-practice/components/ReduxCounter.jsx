e11d1d99ac5891902a8fbcf17dafb06c4aa52139
soongu <naong526@naver.com>
2025-03-05 09:54:47 +0900

    # 23. 스토어에 상태변경 요청하기

--- src/redux-practice/components/ReduxCounter.jsx
+++ src/redux-practice/components/ReduxCounter.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './ReduxCounter.module.css';
 
 const ReduxCounter = () => {
@@ -6,10 +6,30 @@ const ReduxCounter = () => {
   // 리덕스가 관리하는 상태값을 불러오기
   const count = useSelector(state => state.count);
 
+  // 리덕스의 상태변경을 위한 함수 가져오기
+  const dispatch = useDispatch();
+
+  const handleIncrease = e => { 
+    // 카운트를 증가시키려면 redux store에 접근하여 상태값을 변경요청해야함.
+    // 리덕스에서는 상태값 변경을 위해 dispatch함수를 호출해야함.
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const handleDecrease = (e) => {
+    dispatch({ type: 'DECREMENT' });
+  };
+
   return (
     <main className={styles.counter}>
       <h1>Redux Counter</h1>
-      <div className={styles.value}>{ count }</div>
+      <div className={styles.value}>{count}</div>
+      
+      <div style={{
+        marginBottom: 10
+      }}>
+        <button onClick={handleIncrease}>Increment</button>
+        <button onClick={handleDecrease}>Decrement</button>
+      </div>
       <button>Toggle Counter</button>
     </main>
   );
