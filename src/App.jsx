b16b2379b268d387f321f429605a10e7482a5e15
soongu <naong526@naver.com>
2025-03-05 11:24:48 +0900

    # 28. 새 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css';
+import Auth from './redux-practice/components/Auth';
+import Header from './redux-practice/components/Header';
 import ReduxCounter from './redux-practice/components/ReduxCounter';
+import UserProfile from './redux-practice/components/UserProfile';
 
 const App = () => {
   
   return (
     <>
+      <Header />
+      <Auth />
+      {/* <UserProfile /> */}
       <ReduxCounter />
     </>
   );
